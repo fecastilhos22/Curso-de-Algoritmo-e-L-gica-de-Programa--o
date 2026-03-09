370ffd00c1a0eafa6cb4020c145206ab9c99d14b
Bowie <fecastilhos@gmail.com>
2026-03-09 14:59:58 -0300

    aulas de funcao

---
+++ funcoes/teste.js
@@ -0,0 +1,16 @@
+//Exercício 1
+/*
+let contador = 1;
+
+while (contador < 11) {
+  console.log('Executando a interação ' + contador);
+  contador = contador + 1;
+}
+*/
+//Exercício 2
+let contador = 11;
+
+while (contador > 1) {
+  console.log('Executando a interação ' + contador);
+  contador = contador--;
+}
