e6766acc5a7b426358e50959405ac71a20498037
Bowie <fecastilhos@gmail.com>
2026-03-18 14:40:07 -0300

    ultimas aulas

---
+++ funcoes/funcao_como_param_1.js
@@ -0,0 +1,18 @@
+function executar (funcao) {
+    if (typeof funcao === "function"){
+        console.log(funcao()); //Parte central da aula
+    }
+}
+
+function bomDia (){
+    console.log("Bom dia");
+}
+
+executar(3);
+executar(bomDia);// Parte central da aula
+
+const x = bomDia;
+const y = bomDia();
+
+console.log(x());
+console.log(y);
